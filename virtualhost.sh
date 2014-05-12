ac1ec3911d4064e20a7aa3b3e198101da64b0e4d
Jota Miller <HenuXmail@gmail.com>
2014-05-11 23:04:49 -0400

    Se agregan esquemas de virtual host

--- virtualhost.sh
+++ virtualhost.sh
@@ -18,6 +18,8 @@ DOMINIO=''
 #Representa la extensión que el usuario le quera dar al dominio, Ej: .dev, .local
 EXT_DOMINIO=''
 
+
+
 echo -n "Tiene un directorio 'public_html' personal? (Y/n): "
 read DIRECTORIO_USUARIO
 DIRECTORIO_USUARIO=${DIRECTORIO_USUARIO^^}
@@ -40,6 +42,7 @@ else
   RUTA='/var/www/'
 fi
 
+
 # Nombre de dominio
 echo -n "Ingrese el nombre de dominio a crear: "
 read DOMINIO
@@ -52,6 +55,12 @@ read EXT_DOMINIO
 echo -n "La ruta '$RUTA$DOMINIO' existe? (Y/n)"
 read EXISTE_DIRECTORIO
 EXISTE_DIRECTORIO=${EXISTE_DIRECTORIO^^}
+
+# Seleccionar tipo de proyecto
+echo "¿Que tipo de proyecto esta creando?"
+echo " 1 - Estandar"
+echo " 2 - Symfony2"
+read TIPO_PROYECTO
 
 # Resumen y confirmación
 echo ""
@@ -78,7 +87,9 @@ if [[ $PROCEDER == "Y" ]]; then
   touch /etc/apache2/sites-available/$DOMINIO.conf
      
   #AGREGAMOS EL VIRTUAL HOST
-  echo "<VirtualHost *:80>
+  case $TIPO_PROYECTO in
+    1)
+      echo "<VirtualHost *:80>
     ServerName $DOMINIO.$EXT_DOMINIO
 
     ServerAdmin webmaster@localhost
@@ -87,7 +98,54 @@ if [[ $PROCEDER == "Y" ]]; then
     ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
     
-  </VirtualHost>" >  /etc/apache2/sites-available/$DOMINIO.conf
+  </VirtualHost>" > /etc/apache2/sites-available/$DOMINIO.conf
+    ;;
+    2)
+      echo "<VirtualHost *:80>
+  ServerName      $DOMINIO.$EXT_DOMINIO
+  ServerAlias     www.$DOMINIO.$EXT_DOMINIO
+
+  SetEnv SYMFONY__DATABASE__USER     \"...\"
+  SetEnv SYMFONY__DATABASE__PASSWORD \"...\"
+
+  DocumentRoot    \"$RUTA$DOMINIO/web\"
+  DirectoryIndex  app.php
+
+  <Directory \"$RUTA$DOMINIO/web\">
+    AllowOverride None
+    Allow from All
+
+    <IfModule mod_rewrite.c>
+      Options -MultiViews
+      RewriteEngine On
+      RewriteCond %{REQUEST_FILENAME} !-f
+      RewriteRule ^(.*)$ app.php [QSA,L]
+    </IfModule>
+  </Directory>
+
+
+  KeepAlive            On
+  MaxKeepAliveRequests 200
+  KeepAliveTimeout     5
+
+  AddOutputFilterByType DEFLATE text/css text/plain text/html application/xhtml+xml text/xml application/xml
+
+  <IfModule mod_headers.c>
+    Header append Vary User-Agent env=!dont-vary
+
+    ExpiresActive On
+    ExpiresDefault \"now plus 1 week\"
+    ExpiresByType image/x-icon \"now plus 1 month\"
+    ExpiresByType image/gif    \"now plus 1 month\"
+    ExpiresByType image/png    \"now plus 1 month\"
+    ExpiresByType image/jpeg   \"now plus 1 month\"
+  </IfModule>
+</VirtualHost>" > /etc/apache2/sites-available/$DOMINIO.conf
+    ;;
+    *)
+      echo "Opción incorrecta"
+    ;;
+  esac
      
   #CREAMOS EL DIRECTORIO PARA EL DOMINIO
   if [[ $EXISTE_DIRECTORIO == "N" ]]; then
